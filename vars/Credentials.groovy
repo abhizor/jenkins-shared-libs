d252ab25be12f44c50fe57dd5cb946ea6f74c2d9
Abhi <147902339+abhizor@users.noreply.github.com>
2026-06-18 21:47:26 +0530

    Update Credentials.groovy

--- vars/Credentials.groovy
+++ vars/Credentials.groovy
@@ -1,1 +1,7 @@
-
+def call(String credentialsId, String usernameVariable, String  passwordVariable, String imagename){
+  withCredentials([usernamePassword(credentialsId: "${credentialsId}", usernameVariable: "${usernameVariable}", passwordVariable: "${passwordVariable}")]){
+    sh "dockerhub -u ${env.${usernameVariable}} -p${env.${passwordVariable}}"
+    sh "dockerhub push ${imagename}"
+    
+  }
+}
